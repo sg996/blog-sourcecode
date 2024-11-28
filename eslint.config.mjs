9668ddfdf872203d15d7c92fc1c8fccba5921c32
wwlight <1942459198@qq.com>
2024-11-28 14:57:32 +0800

    feat: 添加 eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+// eslint.config.mjs
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  rules: {
+    'prefer-exponentiation-operator': 'off',
+    'prefer-template': 'off',
+    'no-extend-native': 'off',
+    'no-sparse-arrays': 'off',
+    'no-prototype-builtins': 'off',
+    'no-new': 'off',
+  },
+})
